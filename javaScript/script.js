1895f92e991ca6a25b65a946f4f6cd68921aff92
Mohammad Ovesh Ansari <92392084+21Ovi@users.noreply.github.com>
2022-05-21 22:44:35 +0530

    Add files via upload

---
+++ javaScript/script.js
@@ -0,0 +1,33 @@
+// Note
+//  Javascript is a high level object oriented, multi paradgim programming language
+
+// Object oriented Programming Languages: Java, C++
+// Functional Orianted Programming Languages: C, Python
+//  Multi-Paradgim Programming Language: JavaScript
+
+////////////////// Variables ///////////////////////////////
+/**
+ * Variable Declaration by using special characters (var, let, const)
+ * assign values to declared variables
+ */
+
+var amit = 8522585;
+let kevin = "Dakoria";
+
+const monal = "shah";
+
+amit = "Chaudhary";
+kevin = "Mali";
+
+amit = "chaudhary";
+
+console.log(123);
+
+console.log(amit, kevin, "monal");
+
+const Test_value21 = 21;
+
+/**
+ * Declare a variable called 'Country','continent' and 'population' and assign their value according to your own country
+ * and log their values (HINT: console.log() )
+ */
