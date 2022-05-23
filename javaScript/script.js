2e8e050ff0486e7d75107e042569ed6803332fe2
Mohammad Ovesh Ansari <92392084+21Ovi@users.noreply.github.com>
2022-05-23 23:06:28 +0530

    Update script.js

--- javaScript/script.js
+++ javaScript/script.js
@@ -9,25 +9,105 @@
 /**
  * Variable Declaration by using special characters (var, let, const)
  * assign values to declared variables
+ 
+ var amit = 8522585;
+ let kevin = "Dakoria";
+ 
+ const monal = "shah";
+ 
+ amit = "Chaudhary";
+ kevin = "Mali";
+ 
+ amit = "chaudhary";
+ 
+ console.log(123);
+ 
+ console.log(amit, kevin, "monal");
+ 
+ const Test_value21 = 21;
  */
-
-var amit = 8522585;
-let kevin = "Dakoria";
-
-const monal = "shah";
-
-amit = "Chaudhary";
-kevin = "Mali";
-
-amit = "chaudhary";
-
-console.log(123);
-
-console.log(amit, kevin, "monal");
-
-const Test_value21 = 21;
 
 /**
  * Declare a variable called 'Country','continent' and 'population' and assign their value according to your own country
  * and log their values (HINT: console.log() )
+ 
+ const country = 'India';
+ const continent = 'Asia';
+ let population = 7.8;
+ 
  */
+////////////////////////////////// Data-Types ///////////////////////////////////////////////
+/**
+ * there are 7 primitives data types
+ *
+ * 1: Number: used for decimal and integer
+ * Ex: let age = 23
+ *
+ * 2: String: Sequence of characters, used for decimal, integers, characters, words etc...
+ * Ex: let name = 'Kevin', "Amit", `Monal`
+ *
+ * 3: Boolean: Logical type that can only be true or false (1 = true, 0 = false);
+ * Ex: var elidgibleForDriving = true;
+ *
+ * 4: undefined: a value which is not defined
+ * Ex: let numbers;
+ *
+ * 5: Null: a empty value
+ * Ex: let kev = null;
+ *
+ * 6: Symbol (ES2015) : value that is unique and cannot be changed
+ *
+ * 7: BigInt (ES2020): an intiger which is larger than the number can hold
+ *
+ * Note: java script has dynamic typing: we do no have to manully define the datatype of the value;
+ *
+ *
+ 
+ const program = "Javascript";
+ const num = 123;
+ const isIdiot = true;
+ console.log(typeof program);
+ console.log(typeof num);
+ console.log(typeof isIdiot);
+ 
+ const mobile = "+91 8511397395";
+ console.log(typeof mobile);
+ 
+ */
+
+/////////////////////////// Basic Operators ///////////////////////////
+
+//Maths operators
+
+/**
+ * 
+ 
+ let x = 10 + 5;
+ console.log(x);
+ 
+ x = x + 5; // x = 15 + 5
+ console.log(x);
+ 
+ //x = x + 5, can be shorten with this syntax:
+ x += 10;
+ console.log(x);
+ 
+ console.log((x -= 10));
+ x++;
+ x++;
+ x--;
+ console.log(x);
+ */
+
+/**
+ * Increment x = x + 1, x++
+ * Decrement x = x - 1, x--
+ */
+
+// Comparision Operators
+
+let y = 10,
+  z = 12;
+console.log(y > z);
+
+// Equality Operators
